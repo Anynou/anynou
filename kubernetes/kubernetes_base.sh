f5c74467e6081aac9874307947032deb81ca7ab6
Josep Anynou <jose.alonso87@hotmail.com>
2019-12-26 16:31:06 +0100

    rectificacion en script

--- kubernetes/kubernetes_base.sh
+++ kubernetes/kubernetes_base.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-echo "Actualizar repositorios"
-add-apt-repository universe
 echo "Añadir repositorios"
+add-apt-repository universe
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
 echo "Instalar kubernetes"

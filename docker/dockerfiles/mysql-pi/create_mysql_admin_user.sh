aa8656cf715817354502076ef7e2772634c6544d
Anynou <jose.alonso0087@gmail.com>
2020-03-20 18:30:10 +0100

    dockerfiles

---
+++ docker/dockerfiles/mysql-pi/create_mysql_admin_user.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+/usr/bin/mysqld_safe > /dev/null 2>&1 &
+
+RET=1
+while [[ RET -ne 0 ]]; do
+    echo "=> Waiting for confirmation of MySQL service startup"
+    sleep 5
+    mysql -uroot -e "status" > /dev/null 2>&1
+    RET=$?
+done
+
+PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
+_word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
+echo "=> Creating MySQL admin user with ${_word} password"
+
+echo $PASS > /root/.my.cnf
+
+mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$PASS'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
+
+
+echo "=> Done!"
+
+mysqladmin -uroot shutdown

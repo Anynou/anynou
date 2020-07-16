569263f1cb5372908ebd68f1936f4f9bc90d0973
Anynou <jose.alonso0087@gmail.com>
2020-07-16 09:49:39 +0200

    script lxd k8s con volumenes

--- lxd/kubernetes-cluster/README.md
+++ lxd/kubernetes-cluster/README.md
@@ -1,3 +1,8 @@
-Despliegue automatizado de kubernetes en contenedores lxd.
-Para que funcione correctamente se ha de crear un perfil con la configuración del fichero k8s-profile.
-También debemos tener instalado kubectl en la máquina en la que estamos realizando el despliegue.
+#Despliegue automatizado de kubernetes en contenedores lxd._
+
+##Requisitos
+Para que funcione correctamente se ha de crear un perfil con la configuración del fichero k8s-profile._
+También debemos tener instalado kubectl en la máquina en la que estamos realizando el despliegue._
+
+###NOTA:_
+Hay 2 scripts de isntalación, uno nos crea un volumen compartido en los workers, para poder crear volúmenes persistentes y el otro no.
---
+++ lxd/kubernetes-cluster/create-volumenes.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Creando master..."
+lxc launch ubuntu:18.04 kmaster --profile k8s
+sleep 10
+echo "Creando worker1..."
+lxc launch ubuntu:18.04 kworker1 --profile k8s
+echo "Creando worker2..."
+lxc launch ubuntu:18.04 kworker2 --profile k8s
+sleep 10
+echo "Montando volumenes"
+echo "worker1 ..."
+lxc config device add kworker1 data1 disk source=/home/anynou/data path=/data/
+echo "worker2 ..."
+lxc config device add kworker2 data2 disk source=/home/anynou/data path=/data/
+echo "Aprovisionando master ..."
+cat bootstrap-ubuntu.sh | lxc exec kmaster bash
+sleep 5
+echo "Aprovisionando worker1..."
+cat bootstrap-ubuntu.sh | lxc exec kworker1 bash
+echo "Aprovisionando worker2..."
+cat bootstrap-ubuntu.sh | lxc exec kworker2 bash
+sleep 5
+echo "Borrando fichero config..."
+rm .kube/config
+echo "Descargando fichero nuevo..."
+lxc file pull kmaster/etc/kubernetes/admin.conf .kube/config
+echo "Comprobando fichero..."
+kubectl cluster-info
+echo "Comprobando nodos..."
+kubectl get nodes
+
+echo "FIN"
